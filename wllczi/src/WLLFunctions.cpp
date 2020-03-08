c8574cdaea6753bd5fd1d001eb9ed278e48cb46e
ptahmose <jbohl@h-quer.de>
2020-03-08 12:04:28 +0100

    ...playing with "WSLINK" -> decided not to go with it

--- wllczi/src/WLLFunctions.cpp
+++ wllczi/src/WLLFunctions.cpp
@@ -1,4 +1,5 @@
 #include "WolframLibrary.h"
+#include "wstp.h"
 
 /* Adds one to the input, returning the result  */
 EXTERN_C DLLEXPORT int demo_I_I( WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) 
@@ -8,5 +9,18 @@ EXTERN_C DLLEXPORT int demo_I_I( WolframLibraryData libData, mint Argc, MArgumen
 	I0 = MArgument_getInteger(Args[0]);
 	I1 = I0 + 1;
 	MArgument_setInteger(Res, I1);
+	return LIBRARY_NO_ERROR;
+}
+
+EXTERN_C DLLEXPORT int getLibraryInfo(WolframLibraryData libData, WSLINK mlp)
+{
+	WSNewPacket(mlp);
+	//WSPutString(mlp, "HelloWorld");
+	WSPutFunction(mlp, "Association", 1);
+	WSPutFunction(mlp, "Rule", 2);
+	WSPutString(mlp, "test");
+	WSPutString(mlp, "quick brown fox");
+	WSEndPacket(mlp);
+	WSFlush(mlp);
 	return LIBRARY_NO_ERROR;
 }
