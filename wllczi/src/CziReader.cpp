08574026d026cfd933e98718907c05d3ef6149d3
ptahmose <jbohl@h-quer.de>
2020-03-14 21:51:39 +0100

    raspi-build problem

--- wllczi/src/CziReader.cpp
+++ wllczi/src/CziReader.cpp
@@ -2,6 +2,7 @@
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
+#include <locale>
 #include <codecvt>
 #include <WolframImageLibrary.h>
 
@@ -12,9 +13,9 @@ using namespace libCZI;
 void CziReader::Open(const std::string& utf8_filename)
 {
     std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> ucs2conv;
-    std::wstring ucs2 = ucs2conv.from_bytes(utf8_filename);
+    std::wstring wstr = ucs2conv.from_bytes(utf8_filename);
 
-    auto stream = libCZI::CreateStreamFromFile(ucs2.c_str());
+    auto stream = libCZI::CreateStreamFromFile(wstr.c_str());
 
     this->reader->Open(stream);
 }
