be8975638b70191d335c26b362b2181679cb9e29
ptahmose <jbohl@h-quer.de>
2023-03-25 22:07:03 +0100

    less warnings, update gtest to 1.13.0

--- wllczi/src/MImageHelper.cpp
+++ wllczi/src/MImageHelper.cpp
@@ -7,19 +7,19 @@ using namespace libCZI;
 {
     switch (pixelType)
     {
-    case PixelType::Gray8:              return 1;
-    case PixelType::Gray16:             return 2;
-    case PixelType::Gray32Float:        return 4;
-    case PixelType::Bgr24:              return 3;
-    case PixelType::Bgr48:              return 6;
-    case PixelType::Bgr96Float:         return 12;
-    case PixelType::Bgra32:             return 4;
-    case PixelType::Gray64ComplexFloat: return 16;
-    case PixelType::Bgr192ComplexFloat: return 48;
-    case PixelType::Gray32:             return 4;
-    case PixelType::Gray64Float:        return 8;
-    default:
-        throw std::invalid_argument("illegal pixeltype");
+        case PixelType::Gray8:              return 1;
+        case PixelType::Gray16:             return 2;
+        case PixelType::Gray32Float:        return 4;
+        case PixelType::Bgr24:              return 3;
+        case PixelType::Bgr48:              return 6;
+        case PixelType::Bgr96Float:         return 12;
+        case PixelType::Bgra32:             return 4;
+        case PixelType::Gray64ComplexFloat: return 16;
+        case PixelType::Bgr192ComplexFloat: return 48;
+        case PixelType::Gray32:             return 4;
+        case PixelType::Gray64Float:        return 8;
+        default:
+            throw std::invalid_argument("illegal pixeltype");
     }
 }
 
@@ -29,23 +29,23 @@ using namespace libCZI;
     int r;
     switch (pixeltype)
     {
-    case PixelType::Gray8:
-        r = (imgLibFunctions->MImage_new2D)(size.w, size.h, 1, MImage_Type_Bit8, MImage_CS_Gray, False, &mimg);
-        break;
-    case PixelType::Gray16:
-        r = (imgLibFunctions->MImage_new2D)(size.w, size.h, 1, MImage_Type_Bit16, MImage_CS_Gray, False, &mimg);
-        break;
-    case PixelType::Bgr24:
-        r = (imgLibFunctions->MImage_new2D)(size.w, size.h, 3, MImage_Type_Bit8, MImage_CS_RGB, True, &mimg);
-        break;
-    case PixelType::Bgr48:
-        r = (imgLibFunctions->MImage_new2D)(size.w, size.h, 3, MImage_Type_Bit16, MImage_CS_RGB, True, &mimg);
-        break;
-    case PixelType::Gray32Float:
-        r = (imgLibFunctions->MImage_new2D)(size.w, size.h, 1, MImage_Type_Real32, MImage_CS_Gray, False, &mimg);
-        break;
-    default:
-        throw std::invalid_argument("Unsupported pixeltype.");
+        case PixelType::Gray8:
+            r = (imgLibFunctions->MImage_new2D)(size.w, size.h, 1, MImage_Type_Bit8, MImage_CS_Gray, False, &mimg);
+            break;
+        case PixelType::Gray16:
+            r = (imgLibFunctions->MImage_new2D)(size.w, size.h, 1, MImage_Type_Bit16, MImage_CS_Gray, False, &mimg);
+            break;
+        case PixelType::Bgr24:
+            r = (imgLibFunctions->MImage_new2D)(size.w, size.h, 3, MImage_Type_Bit8, MImage_CS_RGB, True, &mimg);
+            break;
+        case PixelType::Bgr48:
+            r = (imgLibFunctions->MImage_new2D)(size.w, size.h, 3, MImage_Type_Bit16, MImage_CS_RGB, True, &mimg);
+            break;
+        case PixelType::Gray32Float:
+            r = (imgLibFunctions->MImage_new2D)(size.w, size.h, 1, MImage_Type_Real32, MImage_CS_Gray, False, &mimg);
+            break;
+        default:
+            throw std::invalid_argument("Unsupported pixeltype.");
     }
 
     if (r != 0)
@@ -62,87 +62,96 @@ using namespace libCZI;
 {
     switch (bd->GetPixelType())
     {
-    case PixelType::Bgr24:
-    {
-        ScopedBitmapLocker<IBitmapData*> lckBm{ bd };
-        MImageHelper::ConvertInplace_RGB24_to_BGR24(bd->GetWidth(), bd->GetHeight(), lckBm.stride, lckBm.ptrDataRoi);
-    }
+        case PixelType::Bgr24:
+        {
+            ScopedBitmapLocker<IBitmapData*> lckBm{ bd };
+            MImageHelper::ConvertInplace_RGB24_to_BGR24(bd->GetWidth(), bd->GetHeight(), lckBm.stride, lckBm.ptrDataRoi);
+        }
 
-    break;
-    case PixelType::Bgr48:
-    {
-        ScopedBitmapLocker<IBitmapData*> lckBm{ bd };
-        MImageHelper::ConvertInplace_RGB48_to_BGR48(bd->GetWidth(), bd->GetHeight(), lckBm.stride, lckBm.ptrDataRoi);
-    }
+        break;
+        case PixelType::Bgr48:
+        {
+            ScopedBitmapLocker<IBitmapData*> lckBm{ bd };
+            MImageHelper::ConvertInplace_RGB48_to_BGR48(bd->GetWidth(), bd->GetHeight(), lckBm.stride, lckBm.ptrDataRoi);
+        }
 
-    break;
+        break;
+        default:
+            break;
     }
 }
 
 /*static*/bool MImageHelper::TryGetPixelType(WolframImageLibrary_Functions imgLibFunctions, MImage mimg, libCZI::PixelType* pixelType)
 {
-    auto dt = imgLibFunctions->MImage_getDataType(mimg);
-    auto cs = imgLibFunctions->MImage_getColorSpace(mimg);
+    const auto dt = imgLibFunctions->MImage_getDataType(mimg);
+    const auto cs = imgLibFunctions->MImage_getColorSpace(mimg);
     switch (dt)
     {
-    case MImage_Type_Bit8:
-    {
-        switch (cs)
+        case MImage_Type_Bit8:
         {
-        case MImage_CS_Gray:
-            if (imgLibFunctions->MImage_getChannels(mimg) == 1)
+            switch (cs)
             {
-                if (pixelType != nullptr) { *pixelType = libCZI::PixelType::Gray8; }
-                return true;
+                case MImage_CS_Gray:
+                    if (imgLibFunctions->MImage_getChannels(mimg) == 1)
+                    {
+                        if (pixelType != nullptr) { *pixelType = libCZI::PixelType::Gray8; }
+                        return true;
+                    }
+
+                    break;
+                case MImage_CS_RGB:
+                    if (imgLibFunctions->MImage_interleavedQ(mimg) && imgLibFunctions->MImage_getChannels(mimg) == 3)
+                    {
+                        if (pixelType != nullptr) { *pixelType = libCZI::PixelType::Bgr24; }
+                        return true;
+                    }
+
+                    break;
+                default:
+                    break;
+
             }
-
-            break;
-        case MImage_CS_RGB:
-            if (imgLibFunctions->MImage_interleavedQ(mimg) && imgLibFunctions->MImage_getChannels(mimg) == 3)
-            {
-                if (pixelType != nullptr) { *pixelType = libCZI::PixelType::Bgr24; }
-                return true;
-            }
-
-            break;
-        }
-    }
-
-    break;
-    case MImage_Type_Bit16:
-    {
-        switch (cs)
-        {
-        case MImage_CS_Gray:
-            if (imgLibFunctions->MImage_getChannels(mimg) == 1)
-            {
-                if (pixelType != nullptr) { *pixelType = libCZI::PixelType::Gray16; }
-                return true;
-            }
-
-            break;
-        case MImage_CS_RGB:
-            if (imgLibFunctions->MImage_interleavedQ(mimg) && imgLibFunctions->MImage_getChannels(mimg) == 3)
-            {
-                if (pixelType != nullptr) { *pixelType = libCZI::PixelType::Bgr48; }
-                return true;
-            }
-
-            break;
-        }
-    }
-
-    break;
-    case MImage_Type_Real32:
-    {
-        if (imgLibFunctions->MImage_getChannels(mimg) == 1)
-        {
-            if (pixelType != nullptr) { *pixelType = libCZI::PixelType::Gray32Float; }
-            return true;
         }
 
         break;
-    }
+        case MImage_Type_Bit16:
+        {
+            switch (cs)
+            {
+                case MImage_CS_Gray:
+                    if (imgLibFunctions->MImage_getChannels(mimg) == 1)
+                    {
+                        if (pixelType != nullptr) { *pixelType = libCZI::PixelType::Gray16; }
+                        return true;
+                    }
+
+                    break;
+                case MImage_CS_RGB:
+                    if (imgLibFunctions->MImage_interleavedQ(mimg) && imgLibFunctions->MImage_getChannels(mimg) == 3)
+                    {
+                        if (pixelType != nullptr) { *pixelType = libCZI::PixelType::Bgr48; }
+                        return true;
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        break;
+        case MImage_Type_Real32:
+        {
+            if (imgLibFunctions->MImage_getChannels(mimg) == 1)
+            {
+                if (pixelType != nullptr) { *pixelType = libCZI::PixelType::Gray32Float; }
+                return true;
+            }
+
+            break;
+        }
+        default:
+            break;
     }
 
     return false;
